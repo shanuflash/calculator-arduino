e7d6854d839141c8e4a39497540560721cfaffd5
Shanu Flash #noυ <shanuflash@gmail.com>
2022-06-20 20:29:22 +0530

    ezz

---
+++ Arduino_Calculator.ino
@@ -0,0 +1,120 @@
+
+#include <LiquidCrystal.h>
+#include <Keypad.h>  
+
+LiquidCrystal lcd(0, 1, 2, 3, 4, 5);
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys [ROWS] [COLS] = {
+  {'1', '2', '3', '+'},
+  {'4', '5', '6', '-'},
+  {'7', '8', '9', '*'},
+  {'C', '.', '=', '/'}
+};
+byte rowPins[ROWS] = {13,12,11,10};
+byte colPins[COLS] = {9,8,7,6};
+
+Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
+boolean presentValue = false;
+boolean next = false;
+boolean final = false;
+boolean decimal1 = false, decimal2 = false;
+String n1, n2, nd1=".", nd2=".";
+float answer;
+char op;
+
+void setup()
+{
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("       4x4");
+  lcd.setCursor(0,1);
+  lcd.print("   Calculator");
+  delay(1000);
+  lcd.clear();
+}
+
+void loop(){
+
+  char key = myKeypad.getKey();
+
+  if (key != NO_KEY && (key=='1'||key=='2'||key=='3'||key=='4'||key=='5'||key=='6'||key=='7'||key=='8'||key=='9'))
+  {
+    if (presentValue != true)
+    {
+      n1 = n1 + key;
+      int numLength = n1.length();
+      lcd.setCursor(15 - numLength, 0); //to adjust one whitespace for operator
+      lcd.print(n1);
+    }
+    else 
+    {
+      n2 = n2 + key;
+      int numLength = n2.length();
+      lcd.setCursor(15 - numLength, 1);
+      lcd.print(n2);
+      final = true;
+    }
+  }
+
+  else if (decimal1 == false && final == false && key != NO_KEY && key=='.')
+  {
+    n1=n1+nd1;
+    int numLength = n1.length();
+    lcd.setCursor(15 - numLength, 0);
+    lcd.print(n1);
+    decimal1=true;
+  }
+  
+  else if (decimal2 == false && final == true && key != NO_KEY && key=='.')
+  {
+    n2=n2+nd2;
+    int numLength = n2.length();
+    lcd.setCursor(15 - numLength, 1);
+    lcd.print(n2);
+    decimal2=true;
+  }
+
+  else if (presentValue == false && key != NO_KEY && (key == '/' || key == '*' || key == '-' || key == '+'))
+  {
+    if (presentValue == false)
+    {
+      presentValue = true;
+      op = key;
+      lcd.setCursor(15,0);
+      lcd.print(op);
+    }
+  }
+
+  else if (final == true && key != NO_KEY && key == '='){
+    if (op == '+'){
+      answer = n1.toFloat() + n2.toFloat();
+    }
+    else if (op == '-'){
+      answer = n1.toFloat() - n2.toFloat();
+    }
+    else if (op == '*'){
+      answer = n1.toFloat() * n2.toFloat();
+    }
+    else if (op == '/'){
+      answer = n1.toFloat() / n2.toFloat();
+    }    
+      lcd.clear();
+      lcd.setCursor(15,0);
+      lcd.autoscroll();
+      lcd.print(answer);
+      lcd.noAutoscroll();
+  }
+  else if (key != NO_KEY && key == 'C'){
+    lcd.clear();
+    presentValue = false;
+    final = false;
+    n1 = "";
+    n2 = "";
+    answer = 0;
+    op = ' ';
+  }
+}
