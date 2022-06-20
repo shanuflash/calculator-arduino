9fbe5d5ff168ab1239ea9b2aada5d1db1d79ad1f
Shanu Flash #noυ <shanuflash@gmail.com>
2022-06-20 21:53:48 +0530

    Update arduino-simplecalc.ino

--- arduino-simplecalc.ino
+++ arduino-simplecalc.ino
@@ -1,78 +1,61 @@
 #include <LiquidCrystal.h>
-#include <Keypad.h>
+#include <Keypad.h>  
 
 LiquidCrystal lcd(0, 1, 2, 3, 4, 5);
 const byte ROWS = 4;
 const byte COLS = 4;
 
-char keys[ROWS][COLS] = {
-    {'1', '2', '3', '+'},
-    {'4', '5', '6', '-'},
-    {'7', '8', '9', '*'},
-    {'C', '.', '=', '/'}};
-byte rowPins[ROWS] = {13, 12, 11, 10};
-byte colPins[COLS] = {9, 8, 7, 6};
+char keys [ROWS] [COLS] = {
+  {'1', '2', '3', '+'},
+  {'4', '5', '6', '-'},
+  {'7', '8', '9', '*'},
+  {'C', '0', '=', '/'}
+};
+byte rowPins[ROWS] = {13,12,11,10};
+byte colPins[COLS] = {9,8,7,6};
 
-Keypad myKeypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
 
 boolean presentValue = false;
+boolean next = false;
 boolean final = false;
-boolean hasdecimal = false;
-boolean decimal1 = false, decimal2 = false;
-String n1, n2, d = ".";
-float answer;
+String num1, num2;
+int answer;
 char op;
 
 void setup()
 {
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
   lcd.print("       4x4");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0,1);
   lcd.print("   Calculator");
   delay(2500);
   lcd.clear();
 }
 
-void loop()
-{
+void loop(){
+
   char key = myKeypad.getKey();
 
-  if (key != NO_KEY && (key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9'))
+  if (key != NO_KEY && (key=='1'||key=='2'||key=='3'||key=='4'||key=='5'||key=='6'||key=='7'||key=='8'||key=='9'||key=='0'))
   {
     if (presentValue != true)
     {
-      n1 = n1 + key;
-      int numLength = n1.length();
-      lcd.setCursor(15 - numLength, 0);
-      lcd.print(n1);
+      num1 = num1 + key;
+      int numLength = num1.length();
+      lcd.setCursor(15 - numLength, 0); //to adjust one whitespace for operator
+      lcd.print(num1);
     }
-    else
+    else 
     {
-      n2 = n2 + key;
-      int numLength = n2.length();
+      num2 = num2 + key;
+      int numLength = num2.length();
       lcd.setCursor(15 - numLength, 1);
-      lcd.print(n2);
+      lcd.print(num2);
       final = true;
     }
-  }
-
-  else if (decimal1 == false && final == false && key != NO_KEY && key == '.')
-  {
-    n1 = n1 + d;
-    int numLength = n1.length();
-    lcd.setCursor(15 - numLength, 0);
-    lcd.print(n1);
-    decimal1 = true;
-  }
-
-  else if (decimal2 == false && final == true && key != NO_KEY && key == '.')
-  {
-    n2 = n2 + d;
-    int numLength = n2.length();
-    lcd.setCursor(15 - numLength, 1);
-    lcd.print(n2);
-    decimal2 = true;
   }
 
   else if (presentValue == false && key != NO_KEY && (key == '/' || key == '*' || key == '-' || key == '+'))
@@ -81,72 +64,36 @@ void loop()
     {
       presentValue = true;
       op = key;
-      lcd.setCursor(15, 0);
+      lcd.setCursor(15,0);
       lcd.print(op);
     }
   }
 
-  else if (final == true && key != NO_KEY && key == '=')
-  {
-    if (op == '+')
-    {
-      answer = n1.toFloat() + n2.toFloat();
-      int answerint=int(answer);
-      if(answer-answerint != 0.00)
-      {
-        hasdecimal = true;
-      }
+  else if (final == true && key != NO_KEY && key == '='){
+    if (op == '+'){
+      answer = num1.toInt() + num2.toInt();
     }
-    else if (op == '-')
-    {
-      answer = n1.toFloat() - n2.toFloat();
-      int answerint=int(answer);
-      if(answer-answerint != 0.00)
-      {
-        hasdecimal = true;
-      }
+    else if (op == '-'){
+      answer = num1.toInt() - num2.toInt();
     }
-    else if (op == '*')
-    {
-      answer = n1.toFloat() * n2.toFloat();
-      int answerint=int(answer);
-      if(answer-answerint != 0.00)
-      {
-        hasdecimal = true;
-      }
+    else if (op == '*'){
+      answer = num1.toInt() * num2.toInt();
     }
-    else if (op == '/')
-    {
-      answer = n1.toFloat() / n2.toFloat();
-      int answerint=int(answer);
-      if(answer-answerint != 0.00)
-      {
-        hasdecimal = true;
-      }
-    }
-    
-    lcd.clear();
-    lcd.setCursor(15, 0);
-    lcd.autoscroll();
-    if(hasdecimal != true)
-    {
-      int answerint=int(answer);
-      lcd.print(answerint);
-    }
-    else
+    else if (op == '/'){
+      answer = num1.toInt() / num2.toInt();
+    }    
+      lcd.clear();
+      lcd.setCursor(15,0);
+      lcd.autoscroll();
       lcd.print(answer);
-    lcd.noAutoscroll();
+      lcd.noAutoscroll();
   }
-  else if (key != NO_KEY && key == 'C')
-  {
+  else if (key != NO_KEY && key == 'C'){
     lcd.clear();
     presentValue = false;
     final = false;
-    decimal1 = false;
-    decimal2 = false;
-    hasdecimal = false;
-    n1 = "";
-    n2 = "";
+    num1 = "";
+    num2 = "";
     answer = 0;
     op = ' ';
   }
